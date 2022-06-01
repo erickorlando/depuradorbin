355f2926613ce28698871113841b447065738a7b
Erick Orlando <erickorlando@outlook.com>
2022-06-01 17:39:42 -0500

    Added System.CommandLine package to parse args to program

--- RemoveBinaries/Program.cs
+++ RemoveBinaries/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace RemoveBinaries;
+﻿using System.CommandLine;
+
+namespace RemoveBinaries;
 
 public static class Program
 {
@@ -6,19 +8,47 @@ public static class Program
 
     public static void Main(string[] args)
     {
+        var pathOption = new Option<string>(
+            "--path",
+            getDefaultValue: () => Environment.CurrentDirectory,
+            description: "The path that contains the Solution File"
+        );
+
+        var rootCommand = new RootCommand{
+            pathOption
+        };
+
+        rootCommand.Description = "Depurador Bin @ Erick Orlando";
+        rootCommand.SetHandler((string path) => {
+            MainMethod(path);
+        }, pathOption);
+
+        rootCommand.Invoke(args);
+    }
+
+
+    ///<summary>
+    /// Delete the binary files from a path that contains .NET Projects
+    ///</summary>
+    /// <param name="path">The path that contains the Solution File</param>
+    public static void MainMethod(string path)
+    {
         try
         {
             char separator = '*';
             char border = '-';
             int length = 100;
-            
+
+            if (string.IsNullOrEmpty(path))
+                path = Environment.CurrentDirectory;
+
             Console.WriteLine(new string(separator, length));
-            Console.WriteLine("This utility allows you to delete all binaries from a .NET Solution");
-            Console.WriteLine($"Running under {Environment.OSVersion.VersionString}");
+            Console.WriteLine($"{new string(separator, 10)} This utility allows you to delete all binaries from a .NET Solution");
+            Console.WriteLine($"{new string(separator, 10)} Running under {Environment.OSVersion.VersionString}");
             Console.WriteLine(new string(separator, length));
 
             Console.WriteLine(new string(border, length));
-            Console.WriteLine($"This operation will run on {Environment.CurrentDirectory} do you want to proceed? [y]/n");
+            Console.WriteLine($"This operation will run on {path} do you want to proceed? [y]/n");
             Console.WriteLine(new string(border, length));
 
             var key = Console.ReadLine() ?? string.Empty;
@@ -26,7 +56,7 @@ public static class Program
             if (key.Equals("n", StringComparison.InvariantCultureIgnoreCase))
                 return;
 
-            DeleteFolders(Environment.CurrentDirectory);
+            DeleteFolders(path);
 
             Console.WriteLine("Process Exited with {0} deletions", quantity);
             Console.WriteLine("Erick Orlando © 2022");
@@ -40,11 +70,14 @@ public static class Program
 
     public static void DeleteFolders(string path)
     {
+        var separator = !Environment.OSVersion.VersionString.Contains("Windows", StringComparison.InvariantCultureIgnoreCase) 
+                ? "/" : "\\";
+
         Directory.GetDirectories(path)
             .ToList()
             .ForEach(directory =>
             {
-                if (directory.EndsWith("\\bin") || directory.EndsWith("\\obj"))
+                if (directory.EndsWith($"{separator}bin") || directory.EndsWith($"{separator}obj"))
                 {
                     try
                     {
